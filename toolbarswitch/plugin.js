bccca4fdddbfad4e3bdfdb1056f547b1b9060e91
paulveldema <paul@paul-mint.(none)>
2013-09-09 14:51:51 +0200

    The toolbarswitch icon was missing in compiled/optimized mode

--- toolbarswitch/plugin.js
+++ toolbarswitch/plugin.js
@@ -68,6 +68,8 @@ function switchMe(editor, callback) {
 CKEDITOR.plugins.add('toolbarswitch', {
 	requires: [ 'button', 'toolbar', 'maximize' ],
 	lang: 'af,ar,bg,bn,bs,ca,cs,cy,da,de,el,en,en-au,en-ca,en-gb,eo,es,et,eu,fa,fi,fo,fr,fr-ca,gl,gu,he,hi,hr,hu,id,is,it,ja,ka,km,ko,ku,lt,lv,mk,mn,ms,nb,nl,no,pl,pt,pt-br,ro,ru,si,sk,sl,sq,sr,sr-latn,sv,th,tr,ug,uk,vi,zh,zh-cn', // %REMOVE_LINE_CORE%
+	icons: 'toolbarswitch', // %REMOVE_LINE_CORE%
+	hidpi: true, // %REMOVE_LINE_CORE%
 	init: function (editor) {
 		var lang = editor.lang;
 		var commandFunction = {
@@ -97,8 +99,7 @@ CKEDITOR.plugins.add('toolbarswitch', {
 		editor.ui.addButton && editor.ui.addButton( 'Toolbarswitch', {
 			label: lang.toolbarswitch.toolbarswitch,
 			command: 'toolbarswitch',
-			toolbar: 'tools',
-			icon: CKEDITOR.skin.path() + 'icons/maximize.png'
+			toolbar: 'tools'
 		});
 	}
 });
