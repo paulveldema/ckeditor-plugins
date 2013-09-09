5a8f6a649e0bd6321413668014fc153b9facbbcd
paulveldema <paul@paul-mint.(none)>
2013-09-09 13:24:41 +0200

    Make button label multilanguage.
    Some languages now have the correct label: nl, en, uk, de and fr.
    The content of the rest is the same as 'en'.
    
    Others are free to contribute the translations for the rest.

--- toolbarswitch/plugin.js
+++ toolbarswitch/plugin.js
@@ -67,9 +67,9 @@ function switchMe(editor, callback) {
 
 CKEDITOR.plugins.add('toolbarswitch', {
 	requires: [ 'button', 'toolbar', 'maximize' ],
-
+	lang: 'af,ar,bg,bn,bs,ca,cs,cy,da,de,el,en,en-au,en-ca,en-gb,eo,es,et,eu,fa,fi,fo,fr,fr-ca,gl,gu,he,hi,hr,hu,id,is,it,ja,ka,km,ko,ku,lt,lv,mk,mn,ms,nb,nl,no,pl,pt,pt-br,ro,ru,si,sk,sl,sq,sr,sr-latn,sv,th,tr,ug,uk,vi,zh,zh-cn', // %REMOVE_LINE_CORE%
 	init: function (editor) {
-
+		var lang = editor.lang;
 		var commandFunction = {
 			exec: function( editor ) {
 				if ( editor.config.toolbar == editor.config.maximizedToolbar ) {
@@ -95,7 +95,7 @@ CKEDITOR.plugins.add('toolbarswitch', {
 		command.readOnly = 1;
 
 		editor.ui.addButton && editor.ui.addButton( 'Toolbarswitch', {
-			label: 'Toolbarswitch',
+			label: lang.toolbarswitch.toolbarswitch,
 			command: 'toolbarswitch',
 			toolbar: 'tools',
 			icon: CKEDITOR.skin.path() + 'icons/maximize.png'
